89ae1144ea6b47681a7ea01784d30fc48bed69bc
nicholas <aigberuan6@gmail.com>
2024-07-25 12:29:44 +0100

    edit

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50 // limit each IP to 100 requests per windowMs
+    max: 50 // limit each IP to 50 requests per windowMs
 });
 
 app.use(limiter);
