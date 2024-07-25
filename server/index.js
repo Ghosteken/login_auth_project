70fdbf9a6237bf2a7a74de6094c77f1ad039632e
nicholas <aigberuan6@gmail.com>
2024-07-25 10:11:18 +0100

    secure measures

--- server/index.js
+++ server/index.js
@@ -1,21 +1,54 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
 
-// database connection
+const app = express();
 connection();
 
-// middlewares
+// Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(helmet());
 
-// routes
+
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("This is the backend!!");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
+// Error-Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use(morgan('combined')); //logs req in combined format
+
+
+const ip = process.env.IP || '127.0.0.1';
 const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+
+app.listen(port, ip, () => {
+    console.log(`Listening on http://${ip}:${port}...`);
+});
